dac45005d3d7fad330c493dac20125e7d0f17c7b
PinkRabbit <pink.rabbit.lzb@gmail.com>
2024-08-16 14:55:38 +0800

    refactor: 重新补充 setup 文件内容

--- setup.py
+++ setup.py
@@ -1,28 +1,15 @@
 from setuptools import setup, find_packages
 
-with open("./VaultWardenApi/__init__.py") as f:
-    for line in f.readlines():
-        if line.startswith("version"):
-            delim = '"' if '"' in line else "'"
-            version = line.split(delim)[1]
-            break
-    else:
-        print("Can't find version! Stop Here!")
-        exit(1)
-
-with open('README.md', encoding='UTF-8') as f:
-    long_description = f.read()
-
 setup(
-    name='VaultWardenApi',
-    version=version,
-    url='https://github.com/PinkRabbitLeader/VaultWardenApi',
+    name='vault_warden_cli_tools',
+    version='0.0.1',
+    url='https://github.com/PinkRabbitLeader/VaultWardenCliTools',
     license='MIT',
     author='lzb',
     author_email='1072095696@qq.com',
-    description="为自建VaultWarden服务提供接口封装服务。"
-                "Provide interface encapsulation services for self-built VaultWarden services.",
-    long_description=long_description,
+    description="基于 Bitwarden CLI 工具(`bw`) 封装的 Python 包，为 Vault Warden 提供命令行接口工具。"
+                "A Python package that wraps the Bit Warden CLI tool (`bw`) to provide cli tools for Vault Warden.",
+    long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
     classifiers=[
         "Development Status :: 4 - Beta",
@@ -40,8 +27,8 @@ setup(
         "Typing :: Typed",
     ],
     project_urls={
-        "Source": "https://github.com/PinkRabbitLeader/VaultWardenApi",
-        "Tracker": "https://github.com/PinkRabbitLeader/VaultWardenApi/issues",
+        "Source": "https://github.com/PinkRabbitLeader/VaultWardenCliTools",
+        "Tracker": "https://github.com/PinkRabbitLeader/VaultWardenCliTools/issues",
     },
     python_requires='>=3.6',
     packages=find_packages(where=".", exclude=['test*']),
